cdb83d28040cb7fbb8e0dad2eb3ea5c8816ae788
DevByWater <egaraudy@gmail.com>
2017-06-08 12:47:35 -0600

    Create bin collection and corresponding meteor methods

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/app'
+import { Bins } from '../imports/collections/bin'
 
 Meteor.startup(()=>{
     ReactDOM.render(<App/>, document.querySelector('.render-target'))
--- client/style/main.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  '#login-buttons': {
-    'margin': [{ 'unit': 'px', 'value': 18 }, { 'unit': 'px', 'value': 18 }, { 'unit': 'px', 'value': 18 }, { 'unit': 'px', 'value': 18 }],
-    'fontSize': [{ 'unit': 'px', 'value': 14 }]
-  }
-});
